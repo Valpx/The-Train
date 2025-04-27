ef6e152d0ff10b40f8105816031b8fcb478c4a84
valentin <valentin.lepreux@gmail.com>
2025-04-27 23:19:28 +0200

    Set up the project

---
+++ src/render.cpp
@@ -0,0 +1,16 @@
+#include "../include/render.hpp"
+
+/* Camera settings */
+float angle_theta{45.0}; // Angle between x axis and viewpoint
+float angle_phy{30.0};   // Angle between z axis and viewpoint
+float dist_zoom{30.0};   // Distance between origin and viewpoint
+
+GLBI_Engine myEngine;
+
+void initScene()
+{
+}
+
+void renderScene()
+{
+}
