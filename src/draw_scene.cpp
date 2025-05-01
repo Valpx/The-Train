a3df0f6aec702c94729c0834f3c70ec8fec6287a
valentin <valentin.lepreux@gmail.com>
2025-05-01 17:41:47 +0200

    Add internal and external curved rail

--- src/draw_scene.cpp
+++ src/draw_scene.cpp
@@ -1,4 +1,4 @@
-#include <math.h>
+#include <cmath>
 
 #include "../include/draw_scene.hpp"
 
@@ -11,23 +11,25 @@ float dist_zoom = 25.0f;    // Distance between origin and viewpoint
 static const float CELL_SIZE = 10.0f;
 GLBI_Convex_2D_Shape ground{3};
 
-/* Straight rail */
+/* Rail settings */
 static const float SR = 0.5f;
 static const float POS_X_RAIL1 = 3.0f;
 static const float POS_X_RAIL2 = 7.0f;
-IndexedMesh *straightRail = NULL;
 
-/* Ballast */
+/* Straight rail */
 static const int STRAIGHT_TRACK_BALLAST_COUNT = 5;
-static const float RR = 0.25f;
-static const float BALLAST_X_START = 2.0f;
-static const float BALLAST_X_END = 8.0f;
-IndexedMesh *ballast = NULL;
+IndexedMesh *straightRail = NULL;
 
 /* Curved rail */
 static const int CURVED_TRACK_BALLAST_COUNT = 3;
 GLBI_Convex_2D_Shape innerCurvedRail{3};
 GLBI_Convex_2D_Shape externalCurvedRail{3};
+
+/* Ballast */
+static const float RR = 0.25f;
+static const float BALLAST_X_START = 2.0f;
+static const float BALLAST_X_END = 8.0f;
+IndexedMesh *ballast = NULL;
 
 GLBI_Engine myEngine;
 
@@ -36,9 +38,9 @@ void initGround(const nlohmann::json &data)
     float sizeGrid = data["size_grid"].get<int>() * CELL_SIZE;
     ground.initShape({0.0f, 0.0f, 0.0f,
                       sizeGrid, 0.0f, 0.0f,
-                      sizeGrid, sizeGrid, 0.0f,
-                      0.0f, sizeGrid, 0.0f});
-    ground.changeNature(GL_TRIANGLE_FAN);
+                      0.0f, sizeGrid, 0.0f,
+                      sizeGrid, sizeGrid, 0.0f});
+    ground.changeNature(GL_TRIANGLE_STRIP);
 }
 
 void initStraightRail()
@@ -76,6 +78,134 @@ void initStraightRail()
     straightRail->createVAO();
 }
 
+void add_triangle(std::vector<float> &in_coord, Vector3D a, Vector3D b, Vector3D c)
+{
+    in_coord.emplace_back(a.x);
+    in_coord.emplace_back(a.y);
+    in_coord.emplace_back(a.z);
+
+    in_coord.emplace_back(b.x);
+    in_coord.emplace_back(b.y);
+    in_coord.emplace_back(b.z);
+
+    in_coord.emplace_back(c.x);
+    in_coord.emplace_back(c.y);
+    in_coord.emplace_back(c.z);
+}
+
+void initInternCurvedRail()
+{
+    const int subdivision = 10;
+    std::vector<float> in_coord{};
+
+    for (auto i = 0; i < subdivision; i++)
+    {
+        float angle1 = (M_PI / 2.0f) * i / subdivision;
+        float angle2 = (M_PI / 2.0f) * (i + 1) / subdivision;
+
+        /* Left face */
+        float x1 = (POS_X_RAIL1 - SR / 2.0f) * std::cos(angle1);
+        float y1 = (POS_X_RAIL1 - SR / 2.0f) * std::sin(angle1);
+
+        float x2 = (POS_X_RAIL1 - SR / 2.0f) * std::cos(angle2);
+        float y2 = (POS_X_RAIL1 - SR / 2.0f) * std::sin(angle2);
+
+        add_triangle(in_coord, {x1, y1, 0.0f}, {x1, y1, SR}, {x2, y2, 0.0f});
+        add_triangle(in_coord, {x1, y1, SR}, {x2, y2, SR}, {x2, y2, 0.0f});
+
+        /* Right face */
+        float x3 = (POS_X_RAIL1 + SR / 2.0f) * std::cos(angle1);
+        float y3 = (POS_X_RAIL1 + SR / 2.0f) * std::sin(angle1);
+
+        float x4 = (POS_X_RAIL1 + SR / 2.0f) * std::cos(angle2);
+        float y4 = (POS_X_RAIL1 + SR / 2.0f) * std::sin(angle2);
+
+        add_triangle(in_coord, {x3, y3, 0.0f}, {x3, y3, SR}, {x4, y4, 0.0f});
+        add_triangle(in_coord, {x3, y3, SR}, {x4, y4, SR}, {x4, y4, 0.0f});
+
+        /* Bottom face */
+        add_triangle(in_coord, {x1, y1, 0.0f}, {x3, y3, 0.0f}, {x2, y2, 0.0f});
+        add_triangle(in_coord, {x3, y3, 0.0f}, {x4, y4, 0.0f}, {x2, y2, 0.0f});
+
+        /* Top face */
+        add_triangle(in_coord, {x1, y1, SR}, {x3, y3, SR}, {x2, y2, SR});
+        add_triangle(in_coord, {x3, y3, SR}, {x4, y4, SR}, {x2, y2, SR});
+
+        /* Front face */
+        if (i == 0)
+        {
+            add_triangle(in_coord, {x1, y1, 0.0f}, {x1, y1, SR}, {x3, y3, 0.0f});
+            add_triangle(in_coord, {x3, y3, 0.0f}, {x1, y1, SR}, {x3, y3, SR});
+        }
+
+        /* Back face */
+        if (i == subdivision - 1)
+        {
+            add_triangle(in_coord, {x2, y2, 0.0f}, {x2, y2, SR}, {x4, y4, 0.0f});
+            add_triangle(in_coord, {x4, y4, 0.0f}, {x2, y2, SR}, {x4, y4, SR});
+        }
+    }
+
+    innerCurvedRail.initShape(in_coord);
+    innerCurvedRail.changeNature(GL_TRIANGLES);
+}
+
+void initExternalCurvedRail()
+{
+    const int subdivision = 10;
+    std::vector<float> in_coord{};
+
+    for (auto i = 0; i < subdivision; i++)
+    {
+        float angle1 = (M_PI / 2.0f) * i / subdivision;
+        float angle2 = (M_PI / 2.0f) * (i + 1) / subdivision;
+
+        /* Left face */
+        float x1 = (POS_X_RAIL2 - SR / 2.0f) * std::cos(angle1);
+        float y1 = (POS_X_RAIL2 - SR / 2.0f) * std::sin(angle1);
+
+        float x2 = (POS_X_RAIL2 - SR / 2.0f) * std::cos(angle2);
+        float y2 = (POS_X_RAIL2 - SR / 2.0f) * std::sin(angle2);
+
+        add_triangle(in_coord, {x1, y1, 0.0f}, {x1, y1, SR}, {x2, y2, 0.0f});
+        add_triangle(in_coord, {x1, y1, SR}, {x2, y2, SR}, {x2, y2, 0.0f});
+
+        /* Right face */
+        float x3 = (POS_X_RAIL2 + SR / 2.0f) * std::cos(angle1);
+        float y3 = (POS_X_RAIL2 + SR / 2.0f) * std::sin(angle1);
+
+        float x4 = (POS_X_RAIL2 + SR / 2.0f) * std::cos(angle2);
+        float y4 = (POS_X_RAIL2 + SR / 2.0f) * std::sin(angle2);
+
+        add_triangle(in_coord, {x3, y3, 0.0f}, {x3, y3, SR}, {x4, y4, 0.0f});
+        add_triangle(in_coord, {x3, y3, SR}, {x4, y4, SR}, {x4, y4, 0.0f});
+
+        /* Bottom face */
+        add_triangle(in_coord, {x1, y1, 0.0f}, {x3, y3, 0.0f}, {x2, y2, 0.0f});
+        add_triangle(in_coord, {x3, y3, 0.0f}, {x4, y4, 0.0f}, {x2, y2, 0.0f});
+
+        /* Top face */
+        add_triangle(in_coord, {x1, y1, SR}, {x3, y3, SR}, {x2, y2, SR});
+        add_triangle(in_coord, {x3, y3, SR}, {x4, y4, SR}, {x2, y2, SR});
+
+        /* Front face */
+        if (i == 0)
+        {
+            add_triangle(in_coord, {x1, y1, 0.0f}, {x1, y1, SR}, {x3, y3, 0.0f});
+            add_triangle(in_coord, {x3, y3, 0.0f}, {x1, y1, SR}, {x3, y3, SR});
+        }
+
+        /* Back face */
+        if (i == subdivision - 1)
+        {
+            add_triangle(in_coord, {x2, y2, 0.0f}, {x2, y2, SR}, {x4, y4, 0.0f});
+            add_triangle(in_coord, {x4, y4, 0.0f}, {x2, y2, SR}, {x4, y4, SR});
+        }
+    }
+
+    externalCurvedRail.initShape(in_coord);
+    externalCurvedRail.changeNature(GL_TRIANGLES);
+}
 void initBallast()
 {
     ballast = basicCylinder(BALLAST_X_END - BALLAST_X_START, RR);
@@ -87,6 +217,8 @@ void initScene(const nlohmann::json &data)
     initGround(data);
     initStraightRail();
     initBallast();
+    initInternCurvedRail();
+    initExternalCurvedRail();
 }
 
 void drawGround()
@@ -131,8 +263,17 @@ void drawStraightTrack()
     myEngine.updateMvMatrix();
 }
 
+void drawCurvedRail()
+{
+    myEngine.setFlatColor(0.2f, 0.2f, 0.2f);
+
+    innerCurvedRail.drawShape();
+    externalCurvedRail.drawShape();
+}
+
 void renderScene()
 {
     drawGround();
-    drawStraightTrack();
+    // drawStraightTrack();
+    drawCurvedRail();
 }
